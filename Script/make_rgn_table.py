8edf04052158cf18104543ca03e11a4d11c59dd7
Ferenc Molnar <none>
2024-12-12 18:00:25 -0600

    rename files

--- Script/make_rgn_table.py
+++ Script/make_rgn_table.py
@@ -1,14 +1,14 @@
 i = 0
-with open("rgg_markup.txt", "w") as f:
-    f.write("<table>\n")    
+with open("rgn_markup.txt", "w") as f:
+    f.write("<table>\n")
     for r in range(200):
         if i > 702:
             break
         f.write("  <tr>\n")
         for c in range(4):
             i += 1
-            if i > 702: 
-                break        
+            if i > 702:
+                break
             f.write('    <td> %d <img src="fig/FigSI_RGG_SIN_%d.png" </td>\n' % (i,i))
         f.write("  </tr>\n")
     f.write("</table>\n")
